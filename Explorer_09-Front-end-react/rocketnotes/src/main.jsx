76b0408b78def832f82fd7abfe7dec47217d96ec
Lucas Prado <lucas.rodrigues.prado120@gmail.com>
2023-03-12 21:49:32 -0300

    fix imports

--- Explorer_09-Front-end-react/rocketnotes/src/main.jsx
+++ Explorer_09-Front-end-react/rocketnotes/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {ThemeProvider} from 'styled-components'
-import theme from './styles/theme'
-import GlobalStyles from './styles/global'
+import {theme} from './styles/theme'
+import {GlobalStyles} from './styles/global'
 import { Routes } from './routes'
 import { AuthProvider } from './hooks/auth'
 
